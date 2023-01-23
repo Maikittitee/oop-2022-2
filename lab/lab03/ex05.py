83a3977a8efa10e0696e5377cbd36ae2b4ae668a
ktunchar <maikittitee1615@gmail.com>
2023-01-23 23:40:39 +0700

    lab03

--- lab/lab03/ex05.py
+++ lab/lab03/ex05.py
@@ -1,6 +1,19 @@
-def	update_records(dictionary_record : dict, id : int, property : str, value : any):
+def	update_records(dictionary_record : dict, id : int, property : str, value : str):
+	if value == "":
+		if (id not in dictionary_record.keys()):
+			return (f"ID:{id} not found")
+		elif (property not in dictionary_record[id].keys()):
+			return (f"Property:{property} not found")
+		else:
+			dictionary_record[id].pop(property)
+		return (dictionary_record)
 	if id not in dictionary_record.keys():
 		dictionary_record.update({id:{property:value}})
+	if property == "tracks":
+		if "tracks" not in dictionary_record[id].keys():
+			dictionary_record.update({id:{"tracks":[value]}})
+		else:
+			dictionary_record[id]["tracks"].append(value)
 	else:
 		dictionary_record[id].update({property:value})
 	return (dictionary_record)
@@ -24,4 +37,4 @@ record_collection = {
 }}
 
 
-print(update_records(record_collection,1,"tracks",["Rick roll", "Sunshine"]))
+print(update_records(record_collection,5439,"albumTitle",""))
