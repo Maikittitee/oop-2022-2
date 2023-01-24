a5a7508cca8ef337ec1d80cd6f565f476c2a8571
ktunchar <maikittitee1615@gmail.com>
2023-01-24 15:16:24 +0700

    done

--- lab/lab03/ex03.py
+++ lab/lab03/ex03.py
@@ -1,9 +1,12 @@
-def	is_plusone_dictionary(d):
-	for ele in d:
-		if ele + 1 != d[ele]:
-			return (False)
-	return (True)
+def is_plusone_dictionary(d):
+  temp = 0
+  for a, b in d.items():
+    if a + 1 != b or temp + 1 != a:
+      return (False)
+    temp = b
+  return (True)
 
-di = {1:2,3:10,5:6,7:8}
+
+di = {1: 2, 3: 4, 5: 6, 7: 8}
 
 print(is_plusone_dictionary(di))
