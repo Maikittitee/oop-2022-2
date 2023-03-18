a60e53f6d12f8bff916e5b9c693aace2662379ea
ktunchar <maikittitee1615@gmail.com>
2023-03-18 12:36:54 +0700

    Create Attribute ProductWithChoice, Promotion, DisplayProduct, Favorite

--- product.py
+++ product.py
@@ -1,3 +1,5 @@
+#Todo : Relation and Method
+
 class Product():
 	def __init__(self, product_id, serial_id, name, price, rating, small_detail, full_detail, detail_img, preview_img, stock):
 		self.product_id = product_id
@@ -16,4 +18,22 @@ class ProductCatalog():
 	def __init__ (self, last_update, list_of_product):
 		self.last_update = last_update
 		self.list_of_product = list_of_product
-	
+
+class ProductWithChoice(Product):
+	def __init__ (self, choice):
+		self.choice = choice
+
+class Promotion(ProductWithChoice):
+	def __init__ (self, time_start, time_end, percent_discount):
+		self.time_start = time_start
+		self.time_end = time_end
+		self.percent_discount = percent_discount
+
+class DisplayProduct(Promotion):
+	def __init__ (self, price_after_discount, feed_type):
+		self.price_after_discount = price_after_discount
+		self.feed_type = feed_type
+
+class Favorite():
+	def __init__ (self)
+
