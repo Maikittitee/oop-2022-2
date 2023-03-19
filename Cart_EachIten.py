55736b7936c2e2145ae1d97e2258528a4b38aa28
jjxwaaa <110412162+jjxwaaa@users.noreply.github.com>
2023-03-19 18:17:26 +0700

    Add files via upload

---
+++ Cart_EachIten.py
@@ -0,0 +1,20 @@
+class Cart() :
+    def __init__(self,  sum_quantity, total_amount):
+        self.sum_quantity = sum_quantity
+        self.total_amount = total_amount
+        self.get_cart = get_cart
+    def add_to_cart():
+        pass
+
+    def update_cart():
+        pass
+    
+    def get_cart():
+        pass
+
+
+class EachItem() :
+    def __init__(self, select_quantity):
+        self.select_quantity = select_quantity
+    def get_eachitem():
+        pass
